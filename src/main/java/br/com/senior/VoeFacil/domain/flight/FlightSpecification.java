0d9485526521044d412721ef3b0b73b1a4c66fe3
cairo <cairo.andrade777@gmail.com>
2023-12-29 14:59:45 -0300

    Feat: Adding CORS policy

--- src/main/java/br/com/senior/VoeFacil/domain/flight/FlightSpecification.java
+++ src/main/java/br/com/senior/VoeFacil/domain/flight/FlightSpecification.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Component
 public class FlightSpecification {
